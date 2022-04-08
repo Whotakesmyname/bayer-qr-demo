08c03665b901613d0c5ec57583a0861f54fb44a0
Chen, Hao <darkray@126.com>
2022-04-08 08:15:46 -0400

    [feat] implemented scanner
    Signed-off-by: Chen, Hao <darkray@126.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { Button, Col, Row } from 'antd';
+import { Button, Col, List, Row, Typography } from 'antd';
 import React from 'react';
+import {useState} from 'react';
 import './App.css';
 import {
   Routes,
   Route,
   Link
 } from "react-router-dom";
+import {QrReader} from 'react-qr-reader';
 
 export default function App() {
   return (
     <div className='App'>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='scanner' element={<Scanner />} />
       </Routes>
     </div>
   );
@@ -26,3 +29,54 @@ function Home() {
   </Row>;
 }
 
+interface WhitePageData {
+  serial_number: string;
+  license: string;
+  computer_name: string;
+  injector_type: string;
+  firmware: string;
+  display_type: string;
+}
+
+const encoder = (input: WhitePageData) => {
+  return JSON.stringify(input);
+}
+
+const decoder = (input: string) => {
+  let result : WhitePageData = JSON.parse(input);
+  return result;
+}
+
+const Scanner = (props: {}) => {
+  const [data, setData] = useState([['No result']]);
+
+  return (
+    <>
+      <Row justify='center' align='middle' style={{minHeight: '100vh'}}>
+        <Col span={16}>
+          <QrReader
+            onResult={(result, error) => {
+              if (!!result) {
+                let text = result?.getText();
+                setData(Object.entries(decoder(text)));
+              }
+              if (!!error) {
+                console.log(error);
+              }
+            }}
+            constraints={{facingMode: {ideal: "environment"}}}
+            containerStyle={{width: '100%'}}
+          />
+
+          <List
+            header={<div>Result</div>}
+            footer={<div>End</div>}
+            bordered
+            dataSource={data}
+            renderItem={item => (<List.Item><Typography.Text mark>{item[0]}: </Typography.Text>{item[1]}</List.Item>)}
+          />
+        </Col>
+      </Row>
+    </>
+  )
+}
