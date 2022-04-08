64ac944191150feccbac987f8032c50ab48ecd85
Chen, Hao <darkray@126.com>
2022-04-08 08:59:49 -0400

    workaround
    Signed-off-by: Chen, Hao <darkray@126.com>

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,14 @@ const decoder = (input: string) => {
 }
 
 const Scanner = (props: {}) => {
-  const [data, setData] = useState([['No result']]);
+  const [data, setData] = useState(Object.entries({
+                  serial_number: "TPAB123456",
+                  license: "00-01-02-AB-CD",
+                  computer_name: "M12345678-TPAB123456",
+                  injector_type: "Stellant2",
+                  firmware: "SR-CRU-123.45-C1.02_WIN_OS;S2-EFG-100.23-A0.01_XY_AJ;BAR-XYZF-1.01_OK",
+                  display_type: "black TCRU",
+                }));
 
   return (
     <>
@@ -61,7 +68,14 @@ const Scanner = (props: {}) => {
             onResult={(result, error) => {
               if (!!result) {
                 let text = result?.getText();
-                setData(Object.entries(decoder(text)));
+                setData(Object.entries({
+                  serial_number: "TPAB123456",
+                  license: "00-01-02-AB-CD",
+                  computer_name: "M12345678-TPAB123456",
+                  injector_type: "Stellant2",
+                  firmware: "SR-CRU-123.45-C1.02_WIN_OS;S2-EFG-100.23-A0.01_XY_AJ;BAR-XYZF-1.01_OK",
+                  display_type: "black TCRU",
+                }));
               }
               if (!!error) {
                 console.log(error);
@@ -114,7 +128,9 @@ const WhitePage = (props: {}) => {
                 <div>Display Type: black TCRU</div>
               </Col>
               <Col span={8}>
-                <QrCode value={encoder(data)} />
+                <div style={{background: 'white', padding: '16px'}}>
+                  <QrCode value={encoder(data)} />
+                </div>
               </Col>
             </Row>
           </Content>
