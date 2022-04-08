af1096d582ca99443b80a5d0e0f18532ac2d6fb6
Chen, Hao <darkray@126.com>
2022-04-08 02:36:06 -0400

    init
    Signed-off-by: Chen, Hao <darkray@126.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
+import { Button, Col, Row } from 'antd';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
+function Home() {
+  return <Row justify="center" align="middle" style={{minHeight: '100vh'}}>
+    <Col span={12}>
+      <Link to="/whitepage"><Button type='primary' size='large' block>White Page Demo</Button></Link>
+      <Link to="/scanner"><Button type='primary' size='large' block>QR Scanner Demo</Button></Link>
+    </Col>
+  </Row>;
+}
+
